7495bc8103392eb318db6d6405703fd0aca3e3e1
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-04-02 03:55:26 +0900

    Enhanced nexecd to execute /bin/echo.
    This nexecd has some problems.
    
    1. This version does not receive command line arguments from nexec. This
    nexecd executes "/bin/echo foo" always.
    2. This nexecd does not write any pid files.
    3. Too slow to show "foo".

--- nexecd/main.c
+++ nexecd/main.c
@@ -1,7 +1,114 @@
+#include <assert.h>
+#include <err.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <fsyscall/start_master.h>
+
+#include <nexec/config.h>
+
+/**
+ * TODO: Share this function with nexec.
+ */
+static void
+die(const char* fmt, ...)
+{
+    char buf[256];
+    snprintf(buf, sizeof(buf), "%s\n", fmt);
+
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, buf, ap);
+    va_end(ap);
+
+    exit(1);
+}
+
+static int
+make_bound_socket(struct addrinfo* ai)
+{
+    int sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sock == -1) {
+        return sock;
+    }
+    int on = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) != 0) {
+        err(1, "setsockopt() failed");
+    }
+    if (bind(sock, ai->ai_addr, ai->ai_addrlen) != 0) {
+        warn("bind() failed");
+        close(sock);
+        return -1;
+    }
+    return sock;
+}
 
 int
-main(int argc, const char* argv[])
+main(/*int argc, char* argv[]*/)
 {
+    struct addrinfo hints;
+    bzero(&hints, sizeof(hints));
+    hints.ai_family = PF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    struct addrinfo* ai;
+    int ecode = getaddrinfo(NULL, "57005", &hints, &ai);
+    if (ecode != 0) {
+        die("getaddrinfo() failed: %s", gai_strerror(ecode));
+    }
+    int sock = -1;
+    struct addrinfo* res;
+    for (res = ai; (res != NULL) && (sock == -1); res = res->ai_next) {
+        sock = make_bound_socket(res);
+    }
+    freeaddrinfo(ai);
+    if (sock == -1) {
+        die("Cannot bind.");
+    }
+    if (listen(sock, 0) != 0) {
+        err(1, "listen() failed");
+    }
+
+    if (daemon(0, 0) != 0) {
+        err(1, "daemon() failed");
+    }
+
+    int fd;
+    while ((fd = accept(sock, NULL, 0)) != -1) {
+        pid_t pid = fork();
+        assert(pid != -1);
+        int status;
+        int wfd;
+        char* args[] = { "/bin/echo", "foo" };
+        switch (pid) {
+        case 0:
+            wfd = dup(fd);
+            if (wfd == -1) {
+                err(1, "dup() failed");
+            }
+#if 0
+            fsyscall_start_master(fd, wfd, argc - 1, argv + 1);
+#endif
+            fsyscall_start_master(fd, wfd, 2, args);
+            /* NOTREACHED */
+            break;
+        default:
+            close(fd);
+            /* TODO: Do not wait child's termination. */
+            waitpid(pid, &status, 0);
+            break;
+        }
+    }
+    close(sock);
+
     return 0;
 }
 
