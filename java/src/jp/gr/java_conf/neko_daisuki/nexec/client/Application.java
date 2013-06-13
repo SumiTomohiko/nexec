28b1a699fb010ca9ef2d0fb53320c72e85b80f7a
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-13 18:14:07 +0900

    Setup the building framework for Java version.

---
+++ java/src/jp/gr/java_conf/neko_daisuki/nexec/client/Application.java
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.neko_daisuki.nexec.client;
+
+public class Application {
+
+    public static void main(String[] args) {
+    }
+}
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
