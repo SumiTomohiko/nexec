d6f39c9297a0f2a751ac65306b0275824827909b
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-09-14 23:33:20 +0900

    Enhanced to send SET_ENVs.

--- java/src/jp/gr/java_conf/neko_daisuki/nexec/client/NexecClient.java
+++ java/src/jp/gr/java_conf/neko_daisuki/nexec/client/NexecClient.java
@@ -5,10 +5,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import jp.gr.java_conf.neko_daisuki.fsyscall.slave.Application;
 import jp.gr.java_conf.neko_daisuki.fsyscall.slave.Links;
@@ -16,19 +20,69 @@ import jp.gr.java_conf.neko_daisuki.fsyscall.slave.Permissions;
 
 public class NexecClient {
 
+    public static class Environment {
+
+        private Map<String, String> mMap;
+
+        public Environment() {
+            mMap = new HashMap<String, String>();
+        }
+
+        public void put(String name, String value) {
+            mMap.put(name, value);
+        }
+
+        public Set<String> nameSet() {
+            return mMap.keySet();
+        }
+
+        public String get(String name) {
+            return mMap.get(name);
+        }
+    }
+
+    private static class StreamPair {
+
+        public InputStream in;
+        public OutputStream out;
+    }
+
+    private static class Buffer {
+
+        private StringBuilder mBuffer;
+
+        public Buffer(String cmd) {
+            mBuffer = new StringBuilder(cmd);
+        }
+
+        public void append(String token) {
+            mBuffer.append(String.format(" %s", token));
+        }
+
+        public void endOfLine() {
+            append("\r\n");
+        }
+
+        public byte[] getBytes() throws UnsupportedEncodingException {
+            return mBuffer.toString().getBytes(ENCODING);
+        }
+    }
+
     private static final String ENCODING = "UTF-8";
 
-    public void run(String server, int port, String[] args, InputStream stdin, OutputStream stdout, OutputStream stderr, Permissions permissions, Links links) throws ProtocolException, InterruptedException, IOException {
+    public void run(String server, int port, String[] args, InputStream stdin, OutputStream stdout, OutputStream stderr, Environment env, Permissions permissions, Links links) throws ProtocolException, InterruptedException, IOException {
         Socket sock = new Socket(server, port);
-        sock.setTcpNoDelay(true);
-
         try {
-            InputStream in = sock.getInputStream();
-            OutputStream out = sock.getOutputStream();
-            doExec(in, out, args);
+            StreamPair pair = new StreamPair();
+            pair.in = sock.getInputStream();
+            pair.out = sock.getOutputStream();
 
+            sendEnvironment(pair, env);
+            doExec(pair, args);
+
+            sock.setTcpNoDelay(true);
             new Application().run(
-                    in, out,
+                    pair.in, pair.out,
                     stdin, stdout, stderr,
                     permissions, links);
         }
@@ -46,13 +100,14 @@ public class NexecClient {
         InputStream stdin = System.in;
         OutputStream stdout = System.out;
         OutputStream stderr = System.err;
+        Environment env = new Environment();
         Permissions perm = new Permissions(true);
         Links links = new Links();
         try {
             client.run(
                     server, port, params,
                     stdin, stdout, stderr,
-                    perm, links);
+                    env, perm, links);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -60,15 +115,25 @@ public class NexecClient {
         }
     }
 
-    private void doExec(InputStream in, OutputStream out, String[] args) throws IOException, ProtocolException {
-        StringBuilder buffer = new StringBuilder("EXEC");
-        for (String a: args) {
-            buffer.append(String.format(" %s", a));
-        }
-        buffer.append("\r\n");
-        out.write(buffer.toString().getBytes(ENCODING));
+    private void doSetEnv(StreamPair pair, String name, String value) throws IOException, ProtocolException {
+        Buffer buffer = new Buffer("SET_ENV");
+        buffer.append(name);
+        buffer.append(value);
+        buffer.endOfLine();
+        pair.out.write(buffer.getBytes());
 
-        readOkOrDie(in);
+        readOkOrDie(pair.in);
+    }
+
+    private void doExec(StreamPair pair, String[] args) throws IOException, ProtocolException {
+        Buffer buffer = new Buffer("EXEC");
+        for (String a: args) {
+            buffer.append(a);
+        }
+        buffer.endOfLine();
+        pair.out.write(buffer.getBytes());
+
+        readOkOrDie(pair.in);
     }
 
     private String readLine(InputStream in) throws IOException, ProtocolException {
@@ -92,6 +157,12 @@ public class NexecClient {
         if (!line.equals("OK")) {
             String fmt = "request was refused: %s";
             throw new ProtocolException(String.format(fmt, line));
+        }
+    }
+
+    private void sendEnvironment(StreamPair pair, Environment env) throws IOException, ProtocolException {
+        for (String name: env.nameSet()) {
+            doSetEnv(pair, name, env.get(name));
         }
     }
 }
